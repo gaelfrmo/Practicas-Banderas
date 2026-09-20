ce19483c0a3417c87ed49c48045b27583ef994e4
Gael Alvarado <gaelalvaradodeavila@gmail.com>
2026-09-19 22:53:42 -0600

    Practica 04: Bandera de Alemania con Column y Box

--- app/src/main/java/com/example/banderas/MainActivity.kt
+++ app/src/main/java/com/example/banderas/MainActivity.kt
@@ -1,20 +1,63 @@
-package com.example.banderascompose
+package com.example.banderas
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Surface
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.banderas.ui.theme.BanderasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BanderasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BanderaAlemania( modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
 
 @Composable
-fun Bandera<NombrePais>(modifier: Modifier = Modifier) {
-    // Aqui va el Row/Column/Box con las franjas o formas
+fun BanderaAlemania(modifier: Modifier = Modifier) {
+    Column(modifier = Modifier){
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize()
+                .background(Color(0xFF000000)),
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+                .background(Color(0xFFDD0000)),
+        )
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize()
+                .background(Color(0xFFFFCE00))
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun Bandera<NombrePais>Preview() {
-    Surface {
-        Bandera<NombrePais>(modifier = Modifier.fillMaxSize())
+fun BanderaAlemaniaVista() {
+    BanderasTheme {
+        BanderaAlemania()
     }
 }
