fcb5f23001adbb2849bce5ffa2089204e165269d
Gael Alvarado <gaelalvaradodeavila@gmail.com>
2026-09-19 22:38:32 -0600

    Practica 01: Bandera de Mexico con Row y Column

--- app/src/main/java/com/example/banderas/MainActivity.kt
+++ app/src/main/java/com/example/banderas/MainActivity.kt
@@ -1,20 +1,97 @@
-package com.example.banderascompose
+package com.example.banderas
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Surface
+
+import androidx.compose.ui.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.banderas.ui.theme.BanderasTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BanderasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BanderaMexico(modifier = Modifier.padding(innerPadding))
+
+                }
+            }
+        }
+    }
+}
 
 @Composable
-fun Bandera<NombrePais>(modifier: Modifier = Modifier) {
-    // Aqui va el Row/Column/Box con las franjas o formas
+fun BanderaMexico(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bandera_de_mexico)
+    Box(modifier = modifier.fillMaxSize()) {
+        Row(modifier = Modifier.fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(color = Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "M", color = Color.Black, fontSize = 90.sp)
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(color = Color.White),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Image(
+                    painter = image,
+                    contentDescription = null,
+                    modifier = Modifier.height(100.dp),
+                    contentScale = ContentScale.Fit
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(color = Color.Red),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "E", color = Color.Black, fontSize = 90.sp)
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun Bandera<NombrePais>Preview() {
-    Surface {
-        Bandera<NombrePais>(modifier = Modifier.fillMaxSize())
+fun BanderaMexicovista() {
+    BanderasTheme {
+        BanderaMexico()
     }
 }
