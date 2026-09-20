6ed8d1987cb46d77624f65fc239dbb98e0fe0324
Gael Alvarado <gaelalvaradodeavila@gmail.com>
2026-09-19 22:44:50 -0600

    Practica 02: Bandera de Francia con Row, Box y Column

--- app/src/main/java/com/example/banderas/MainActivity.kt
+++ app/src/main/java/com/example/banderas/MainActivity.kt
@@ -1,20 +1,73 @@
-package com.example.banderascompose
+package com.example.banderas
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Surface
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.banderas.ui.theme.BanderasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BanderasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BanderaFrancia(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
 
 @Composable
-fun Bandera<NombrePais>(modifier: Modifier = Modifier) {
-    // Aqui va el Row/Column/Box con las franjas o formas
+fun BanderaFrancia(modifier: Modifier = Modifier) {
+    Box(modifier = Modifier){
+        Row(modifier = Modifier.fillMaxSize()){
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(Color(0xFF0055A4)),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){ }
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+            ) { }
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()
+                    .background(Color(0xFFEF4135)),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){ }
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun Bandera<NombrePais>Preview() {
-    Surface {
-        Bandera<NombrePais>(modifier = Modifier.fillMaxSize())
+fun BanderaFranciavista() {
+    BanderasTheme {
+        BanderaFrancia()
     }
 }
