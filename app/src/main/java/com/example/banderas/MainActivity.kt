9613094f72c75b93ed29e4bf93cd7862fff88d6f
Gael Alvarado <gaelalvaradodeavila@gmail.com>
2026-09-22 11:38:48 -0600

    Se agrego un main

--- app/src/main/java/com/example/banderas/MainActivity.kt
+++ app/src/main/java/com/example/banderas/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.example.banderas
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.banderas.ui.theme.BanderasTheme
 
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BanderasTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BanderaColombia(modifier = Modifier.padding(innerPadding))
+
+                }
+            }
+        }
+    }
+}
 @Composable
 fun BanderaColombia(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
